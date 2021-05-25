0fd5d0ab6422a4265de10ee8c7880983880c055c
oassami <76979230+oassami@users.noreply.github.com>
2021-05-24 18:22:54 -0700

    Assignment completed

--- tvshow_app/urls.py
+++ tvshow_app/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('shows', views.shows),
+    path('shows/new', views.create),
+    path('shows/<int:show_id>', views.display),
+    path('shows/<int:show_id>/edit', views.update),
+    path('shows/<int:show_id>/destroy', views.destroy),
 ]
--- tvshow_app/views.py
+++ tvshow_app/views.py
@@ -2,7 +2,47 @@ from django.shortcuts import render, redirect
 from .models import *
 
 def index(request):
+    return redirect('/shows')
+
+def shows(request):
     context={
-        'testing': 'Initial TV SHOW project is working!!!'
+        'all_shows': Show.objects.all()
     }
-    return render(request, "index.html", context)
+    return render(request, "shows.html", context)
+
+def create(request):
+    if request.method=="GET":
+        return render(request, 'new.html')
+    else:
+        show = Show.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'], description=request.POST['description'])
+    return redirect(f'/shows/{show.id}')
+
+def display(request, show_id):
+    context={
+        'tv_show': Show.objects.get(id=show_id)
+    }
+    return render(request, 'display.html', context)
+
+def update(request, show_id):
+    if request.method=="GET":
+        context={
+            'tv_show': Show.objects.get(id=show_id)
+        }
+        return render(request, 'edit.html', context)
+    else:
+        show = Show.objects.get(id=show_id)
+        show.title=request.POST['title']
+        show.network=request.POST['network']
+        show.release_date=request.POST['release_date']
+        show.description=request.POST['description']
+        show.save()
+        context={
+            'tv_show': show
+        }
+    return render(request, 'display.html', context)
+
+def destroy(request, show_id):
+    show = Show.objects.get(id=show_id)
+    print(show.title)
+    show.delete()
+    return redirect('/shows')
