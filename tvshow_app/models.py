b13c9cb0c8bc7dd0b77eb3227be6f12d8fd480f3
oassami <76979230+oassami@users.noreply.github.com>
2021-05-29 21:40:59 -0700

    with request.session

--- tvshow_app/models.py
+++ tvshow_app/models.py
@@ -2,31 +2,53 @@ from django.db import models
 from datetime import date, time, datetime
 
 class ShowManager(models.Manager):
-    def addShowValidation(self, post_date):
+    # def addShowValidation(self, post_data):
+    #     errors = {}
+    #     if len(post_data['title']) < 2:
+    #         errors['title'] = 'Title must be at least 2 characters'
+    #     if len(post_data['network']) < 3:
+    #         errors['network'] = 'Network must be at least 3 characters'
+    #     if len(post_data['description']) > 0 and len(post_data['description']) < 10:
+    #         errors['description'] = 'Description must be at least 10 characters'
+    #     if len(post_data['release_date']) < 6:
+    #         errors['release_date'] = 'Release Date must be valid date.'
+    #     else:
+    #         release_date = date.fromisoformat(post_data['release_date'])
+    #         today_date = date.today()
+    #         if release_date > today_date:
+    #             errors['release_date'] = 'Release Date must be in the past.'
+    #     shows = Show.objects.all()
+    #     for show in shows:
+    #         if show.title == post_data['title']:
+    #             errors['duplicate'] = 'The title already exists in the database!'
+    #             return errors
+    #     return errors
+
+    def showValidation(self, post_data, req_title):
         errors = {}
-        if len(post_date['title']) < 2:
+        if len(post_data['title']) < 2:
             errors['title'] = 'Title must be at least 2 characters'
-        if len(post_date['network']) < 3:
+        if len(post_data['network']) < 3:
             errors['network'] = 'Network must be at least 3 characters'
-        if len(post_date['description']) > 0 and len(post_date['description']) < 10:
+        if len(post_data['description']) > 0 and len(post_data['description']) < 10:
             errors['description'] = 'Description must be at least 10 characters'
-        if len(post_date['release_date']) < 6:
+        if len(post_data['release_date']) < 6:
             errors['release_date'] = 'Release Date must be valid date.'
         else:
-            release_date = date.fromisoformat(post_date['release_date'])
+            release_date = date.fromisoformat(post_data['release_date'])
             today_date = date.today()
             if release_date > today_date:
                 errors['release_date'] = 'Release Date must be in the past.'
-        shows = Show.objects.all()
-        for show in shows:
-            if show.title == post_date['title']:
-                errors['duplicate'] = 'This title already exists in the database!'
+        if req_title != post_data['title']:
+            show = Show.objects.filter(title=post_data['title'])
+            if show:
+                errors['duplicate'] = 'The title already exists in the database!'
         return errors
 
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.CharField(max_length=45)
-    release_date = models.CharField(max_length=20)
+    release_date = models.DateField()
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- tvshow_app/views.py
+++ tvshow_app/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-
+from datetime import date, time, datetime
 from .models import *
 
 def index(request):
+    request.session.clear()
     return redirect('/shows')
 
 def shows(request):
@@ -16,54 +17,57 @@ def create(request):
     if request.method=="GET":
         return render(request, 'new.html')
     else:
-        errors = Show.objects.addShowValidation(request.POST)
+        errors = Show.objects.showValidation(request.POST, '')
+        request.session['title']=request.POST['title']
+        request.session['network']=request.POST['network']
+        request.session['release_date']=request.POST['release_date']
+        request.session['description']=request.POST['description']
         if errors:
             for key, value in errors.items():
                 messages.error(request, value)
-            context = {
-                'title': request.POST['title'],
-                'network': request.POST['network'],
-                'release_date': request.POST['release_date'],
-                'description': request.POST['description']
-            }
-            return render(request, 'new.html', context)
+            return redirect('/shows/new')
         show = Show.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['release_date'], description=request.POST['description'])
     return redirect(f'/shows/{show.id}')
 
 def display(request, show_id):
-    context={
-        'tv_show': Show.objects.get(id=show_id)
-    }
-    return render(request, 'display.html', context)
+    show = Show.objects.get(id=show_id)
+    request.session['id'] = show.id
+    request.session['title'] = show.title
+    request.session['network'] = show.network
+    request.session['release_date'] = str(show.release_date)
+    request.session['description'] = show.description
+    return render(request, 'display.html')
 
 def update(request, show_id):
+    show = Show.objects.get(id=show_id)
     if request.method=="GET":
-        context={
-            'tv_show': Show.objects.get(id=show_id)
-        }
-        return render(request, 'edit.html', context)
+        if 'title' not in request.session:
+            request.session['id'] = show.id
+            request.session['title'] = show.title
+            request.session['network'] = show.network
+            request.session['release_date'] = str(show.release_date)
+            request.session['description'] = show.description
+        return render(request, 'edit.html')
     else:
-        show = Show.objects.get(id=show_id)
+        errors = Show.objects.showValidation(request.POST, show.title)
+        request.session['title']=request.POST['title']
+        request.session['network']=request.POST['network']
+        request.session['release_date']=request.POST['release_date']
+        request.session['description']=request.POST['description']
+        if errors:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect(f'/shows/{show_id}/edit')
         show.title=request.POST['title']
         show.network=request.POST['network']
         show.release_date=request.POST['release_date']
         show.description=request.POST['description']
-        errors = Show.objects.addShowValidation(request.POST)
-        if errors:
-            for key, value in errors.items():
-                messages.error(request, value)
-            context = {
-                'tv_show': show
-            }
-            return render(request, 'edit.html', context)
         show.save()
-        context={
-            'tv_show': show
-        }
-    return render(request, 'display.html', context)
+        request.session.clear()
+    return redirect(f'/shows/{show_id}')
 
 def destroy(request, show_id):
     show = Show.objects.get(id=show_id)
     print(show.title)
     show.delete()
-    return redirect('/shows')
+    return redirect('/')
